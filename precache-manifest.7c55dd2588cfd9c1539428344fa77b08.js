b3a3f2755e50502112513ffaed62d8726742a3fb
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-05-17 21:09:14 -0700

    Updates

---
+++ precache-manifest.7c55dd2588cfd9c1539428344fa77b08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52198446bd1067e3b66c534759ad1463",
+    "url": "/google-books-search-react/index.html"
+  },
+  {
+    "revision": "256169a4b4d6f6f91ad9",
+    "url": "/google-books-search-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d0732b20b6e2aaa6135",
+    "url": "/google-books-search-react/static/js/2.00745468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-books-search-react/static/js/2.00745468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256169a4b4d6f6f91ad9",
+    "url": "/google-books-search-react/static/js/main.315e4bf9.chunk.js"
+  },
+  {
+    "revision": "ab97123f8642055815f7",
+    "url": "/google-books-search-react/static/js/runtime-main.31dc18bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books-search-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
